4db1fed74b73eadf4198cf76529750735bf82c2c
Tsudhishnair <sudhishnair53@gmail.com>
2021-05-04 15:01:53 +0530

    Initialized the project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Clippy App</Text>
+    </SafeAreaView>
+  );
+}
