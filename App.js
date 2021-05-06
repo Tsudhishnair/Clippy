65d982719d83466c4b4099228d7d31b1672e9114
Tsudhishnair <sudhishnair53@gmail.com>
2021-05-06 22:32:07 +0530

    Implemented Stack navigation

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import colors from './app/constants/colors';
 import Clippy from './app/index';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Clippy />
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={styles.topContainer} />
+      <SafeAreaView style={styles.rootContainer}>
+        <Clippy />
+      </SafeAreaView>
+    </>
   );
 }
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+  },
+  topContainer: {
+    flex: 0,
+    backgroundColor: colors.primaryColor,
+  },
+});
